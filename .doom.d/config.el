cf4ff5097da7ef36dfab974f86118a52224799e0
joris <joris.vansteenbrugge@wur.nl>
2022-10-28 11:33:33 +0200

    set default emacs team

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-flatwhite)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -100,3 +100,4 @@
 
 
 ;;(setq-default TeX-master "master") ; All master files called "master"
+(map! "<escape>" #'doom/escape)
